194003a0357a18aa308dda56301e80ef4fd2bc7e
Lauren Hunter <laurenbhunter00@gmail.com>
2022-08-15 14:42:50 +0200

    questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get "/ask", to: "questions#ask"
+  # get "QuestionsController", to "pages#"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get "/answer", to: "questions#answer"
   # root "articles#index"
 end
